22729321d0b3b869cbc6d32d17d1efc412f45811
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-05-04 12:03:57 -0400

    create flyway migrations

---
+++ src/main/resources/db/migration/V1__create_character.sql
@@ -0,0 +1,16 @@
+CREATE TABLE characters (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  bio TEXT NOT NULL,
+  durability INTEGER NOT NULL CHECK (durability > 0 AND durability < 8),
+  energy INTEGER NOT NULL CHECK (energy > 0 AND energy <8),
+  fighting_skills INTEGER NOT NULL CHECK (fighting_skills > 0 AND fighting_skills <8),
+  intelligence INTEGER NOT NULL CHECK (intelligence > 0 AND intelligence < 8),
+  speed INTEGER NOT NULL CHECK (speed > 0 AND speed < 8),
+  strength INTEGER NOT NULL CHECK (strength > 0 AND strength < 8),
+  height float,
+  weight INTEGER,
+  gender VARCHAR(255),
+  eye_color VARCHAR(255),
+  hair_color VARCHAR(255)
+);
