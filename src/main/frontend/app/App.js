8ba2c62efb97afd7205f6a075dca4a0d8e3ab0d6
Andisa Montez <andisa.andisa@gmail.com>
2020-05-06 16:30:32 -0400

    navbar styling

--- src/main/frontend/app/App.js
+++ src/main/frontend/app/App.js
@@ -2,17 +2,58 @@ import React from "react"
 import { BrowserRouter, Switch, Link, Redirect, Route } from "react-router-dom"
 import CharacterListContainer from "./components/CharacterListContainer"
 import CharacterShowContainer from "./components/CharacterShowContainer"
+import NewCharacterForm from "./components/NewCharacterForm"
 
 const App = (props) => {
   return (
     <BrowserRouter>
-      <div>
-        <Link to={"/characters"}>
-          <p>Characters</p>
-        </Link>
+      <div className="top-links">
+        <nav className="top-bar topbar-responsive">
+          <div className="top-bar-title">
+            <Link to="/" className="topbar-responsive-logo">
+              <strong>Marvel Reviews</strong>
+            </Link>
+          </div>
+          <div id="topbar-responsive" className="topbar-responsive-links">
+            <div className="top-bar-right">
+              <ul className="menu simple vertical medium-horizontal">
+                <li>
+                  <Link to="/new">
+                    <button
+                      type="button"
+                      className="button hollow topbar-responsive-button"
+                    >
+                      New
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/characters">
+                    <button
+                      type="button"
+                      className="button hollow topbar-responsive-button"
+                    >
+                      Characters
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <input type="search" placeholder="Search"></input>
+                </li>
+
+                <button
+                  type="button"
+                  className="button hollow top-bar-responsive-button"
+                >
+                  Search
+                </button>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
 
-      <div>
+      <div className="botton">
         <Switch>
           <Redirect exact path="/" to="/characters" />
           <Route exact path="/characters" component={CharacterListContainer} />
@@ -21,6 +62,7 @@ const App = (props) => {
             path="/characters/:id"
             component={CharacterShowContainer}
           />
+          <Route exact path="/new" component={NewCharacterForm} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/main/java/com/launchacademy/giantleap/controllers/api/v1/MarvelCharacterApiController.java
+++ src/main/java/com/launchacademy/giantleap/controllers/api/v1/MarvelCharacterApiController.java
@@ -2,13 +2,17 @@ package com.launchacademy.giantleap.controllers.api.v1;
 
 import com.launchacademy.giantleap.models.MarvelCharacter;
 import com.launchacademy.giantleap.repositories.MarvelCharacterRepository;
+import javax.validation.Valid;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,6 +41,22 @@ public class MarvelCharacterApiController {
     }
   }
 
+  @NoArgsConstructor
+  private class InvalidMarvelCharacterException extends RuntimeException {
+
+  }
+
+  @ControllerAdvice
+  private class InvalidMarvelCharacterAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(InvalidMarvelCharacterException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    String invalidMarvelCharacter(InvalidMarvelCharacterException ex) {
+      return ex.getMessage();
+    }
+  }
+
   @GetMapping("/characters")
   public Iterable<MarvelCharacter> getAllCharacters() {
     return marvelCharacterRepo.findAll();
@@ -45,5 +65,15 @@ public class MarvelCharacterApiController {
   @GetMapping("/characters/{id}")
   public MarvelCharacter getSingleCharacter(@PathVariable Integer id) {
     return marvelCharacterRepo.findById(id).orElseThrow(MarvelCharacterNotFoundException::new);
+  }
+
+  @PostMapping("/new")
+  public MarvelCharacter create(@RequestBody @Valid MarvelCharacter marvelCharacter,
+      BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      throw new InvalidMarvelCharacterException();
+    } else {
+      return marvelCharacterRepo.save(marvelCharacter);
+    }
   }
 }
