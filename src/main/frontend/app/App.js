27c117335e466a3c43dc328d55c7852cdd3b4aff
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-05-05 22:55:14 -0400

    navbar added

--- src/main/frontend/app/App.js
+++ src/main/frontend/app/App.js
@@ -6,13 +6,90 @@ import CharacterShowContainer from "./components/CharacterShowContainer"
 const App = (props) => {
   return (
     <BrowserRouter>
-      <div>
-        <Link to={"/characters"}>
-          <p>Characters</p>
-        </Link>
+      {/* <div className="top-links">
+        <nav className="top-bar topbar-responsive">
+          <div className="top-bar-title">
+            <Link to={"/"} id="topbar-site-title">
+              <strong>Marvel Characters</strong>
+            </Link>
+          </div>
+
+          <div it="topbar-responsive" className="topbar-responsive-links">
+            <div className="top-bar-right">
+              <ul className="menu simple vertical medium-horizontal">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+
+                <li>
+                  <Link to="/characters">Characters</Link>
+                </li>
+
+                <li>
+                  <Link to="/">Add Character</Link>
+                </li>
+
+                <li>
+                  <input type="search" placeholder="Search"></input>
+                </li>
+                <li>
+                  <button type="button" class="button">
+                    Search
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div> */}
+
+      <div className="top-links">
+        <nav class="top-bar topbar-responsive">
+          <div class="top-bar-title">
+            <Link to="/" class="topbar-responsive-logo">
+              <strong>Marvel Reviews</strong>
+            </Link>
+          </div>
+          <div id="topbar-responsive" class="topbar-responsive-links">
+            <div class="top-bar-right">
+              <ul class="menu simple vertical medium-horizontal">
+                <li>
+                  <Link to="/">
+                    <button
+                      type="button"
+                      class="button hollow topbar-responsive-button"
+                    >
+                      New
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/characters">
+                    <button
+                      type="button"
+                      class="button hollow topbar-responsive-button"
+                    >
+                      Characters
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <input type="search" placeholder="Search"></input>
+                </li>
+
+                <button
+                  type="button"
+                  class="button hollow top-bar-responsive-button"
+                >
+                  Search
+                </button>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
 
-      <div>
+      <div className="botton">
         <Switch>
           <Redirect exact path="/" to="/characters" />
           <Route exact path="/characters" component={CharacterListContainer} />
