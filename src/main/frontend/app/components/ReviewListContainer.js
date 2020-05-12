3690b70ef5e8f7092d0406689edf84fd9e2fd4af
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-05-11 21:19:10 -0400

    update to delete confirm

--- src/main/frontend/app/components/ReviewListContainer.js
+++ src/main/frontend/app/components/ReviewListContainer.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react"
 import ReviewInfo from "./ReviewInfo"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogContentText from "@material-ui/core/DialogContentText"
+import DialogTitle from "@material-ui/core/DialogTitle"
+import Button from "@material-ui/core/Button"
 
 const ReviewListContainer = (props) => {
+  const [open, setOpen] = useState(false)
+
   let reviews
   if (props.character.reviews) {
     reviews = props.character.reviews.map((item) => {
@@ -55,41 +63,86 @@ const ReviewListContainer = (props) => {
         ratingName = "5 Stars"
       }
 
+      const handleOpen = () => {
+        setOpen(true)
+      }
+
+      const handleClose = () => {
+        setOpen(false)
+      }
+
+      const openDialog = () => {
+        setOpen(true)
+      }
+
+      let answer = ""
+      const accept = () => {
+        setOpen(false)
+
+        let id = item.id
+        fetch(`/api/v1/delete/${id}`, {
+          method: "DELETE",
+          body: JSON.stringify(item),
+          headers: { "Content-Type": "application/json" },
+        })
+          .then((response) => {
+            if (response.ok) {
+              props.deleted()
+            } else {
+              let errorMessage = `${response.status} (${response.statusText})`
+              throw new Error(errorMessage)
+            }
+          })
+          .catch((error) => console.error(`Error in fetch: ${error.message}`))
+      }
+
+      const cancel = () => {
+        setOpen(false)
+      }
+
       const deleteReview = (event) => {
         event.preventDefault()
-
-        let answer = prompt(
-          "Are you sure you want to delete this review?\nEnter 'Yes' to delete or 'No' to cancel"
-        )
-        if (answer.toLocaleLowerCase() === "yes") {
-          let id = item.id
-          fetch(`/api/v1/delete/${id}`, {
-            method: "DELETE",
-            body: JSON.stringify(item),
-            headers: { "Content-Type": "application/json" },
-          })
-            .then((response) => {
-              if (response.ok) {
-                props.deleted()
-              } else {
-                let errorMessage = `${response.status} (${response.statusText})`
-                throw new Error(errorMessage)
-              }
-            })
-            .catch((error) => console.error(`Error in fetch: ${error.message}`))
-        }
+        openDialog()
       }
 
       return (
-        <ReviewInfo
-          key={item.id}
-          review={item}
-          character={props.character}
-          ratingIcon={ratingIcon}
-          ratingName={ratingName}
-          delete={deleteReview}
-          edited={props.edited}
-        />
+        <div>
+          <ReviewInfo
+            key={item.id}
+            review={item}
+            character={props.character}
+            ratingIcon={ratingIcon}
+            ratingName={ratingName}
+            delete={deleteReview}
+            edited={props.edited}
+          />
+
+          <div>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">
+                {"Delete Review"}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Are you sure you want to delete this review?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={cancel} color="primary">
+                  No
+                </Button>
+                <Button onClick={accept} color="primary">
+                  Yes
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
+        </div>
       )
     })
   }
